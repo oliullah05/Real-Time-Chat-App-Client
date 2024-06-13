512931ccbb300a399fe22933dce24a4b86400cdc
oliullah05 <mdolihasan@gmail.com>
2024-06-14 01:21:35 +0600

    logout and settings initial design done

--- src/component/Home/settings/Settings.tsx
+++ src/component/Home/settings/Settings.tsx
@@ -1,9 +1,41 @@
+import { CgLogOut } from "react-icons/cg";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+
+
+
+import { useAppDispatch } from "../../../redux/hooks";
+import { logOut } from "../../../redux/features/auth/authSlice";
+
 
 const Settings = () => {
+const dispatch = useAppDispatch()
+
+
+
+const handleLogout = ()=>{
+dispatch(logOut())
+}
+
     return (
-        <div className=" basis-[5%]">
-            Settings
-        </div>
+        <section className="bg-[#FFFFFF]   basis-[5%]">
+
+            <div className="h-full flex flex-col justify-between items-center  pb-5">
+                <div className=" w-full py-5">
+              
+                </div>
+
+
+               <div className="flex flex-col gap-5">
+               <CgProfile className="h-7 w-7 hover:text-[#20c996eb]" />
+                <IoSettingsOutline className="h-7 w-7 hover:text-[#20c996eb]" />
+                <CgLogOut onClick={handleLogout} className="h-7 w-7 hover:text-[#20c996eb]" />
+               </div>
+            </div>
+
+          
+
+        </section>
     );
 };
 
